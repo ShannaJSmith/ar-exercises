1ee598c197378de146937b9be01f448d2bffaefd
ShannaJSmith <shanna_smith987@hotmail.com>
2021-11-09 02:45:17 +0000

    add validations to employees and store classes

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,22 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Employee < ApplicationRecord
+  validates :first_name, prescence: true
+  validates :last_name, prescence: true
+  validates hourly_rate, numericality: {only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200}
+  # could also try :inclusion => 40..200
+  validates :store_id, prescence: true
+end
+
+class Store < ApplicationRecord
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true }  # could also use greater_than_or_equal_to: 0
+  validate :must_have_clothes
+
+  def must_have_clothes
+    if mens_apparel == false && womens_apparel == false
+      errors.add(:must_have_clothes, "Stores must have mens or womens apparel")
+    end
+  end
+end
